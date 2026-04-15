2018092b919f872ee3655981887ec73bcb88262a
Yogesh Magar <magaryogesh47783@gmail.com>
2026-04-15 21:11:48 +0530

    first commit

---
+++ agent.py
@@ -0,0 +1,22 @@
+from strands import Agent
+from strands.models.gemini import GeminiModel
+from strands_tools import http_request
+
+model = GeminiModel(
+    client_args={
+        "api_key": "AIzaSyDcVvhdhZ3UzLHOrwvndbdpWRnPZcy2_6M",
+    },
+    # **model_config
+    model_id="gemini-3.1-flash-lite-preview",
+    params={
+        # some sample model parameters
+        "temperature": 0.7,
+        "max_output_tokens": 2048,
+        "top_p": 0.9,
+        "top_k": 40
+    }
+)
+
+agent = Agent(model=model, tools=[http_request])
+response = agent("What is 2+2")
+print(response)
